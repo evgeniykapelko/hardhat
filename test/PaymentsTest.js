e9e1ffc6a2ee4a7b790cc452d2b48a375c5b4eae
Eugene Kapelko <evgeniy.kapelko.c@balanceinternet.com.au>
2023-05-15 12:35:39 +0300

    Work with hardhat, exists one not resolved issue with function pay

--- test/PaymentsTest.js
+++ test/PaymentsTest.js
@@ -2,5 +2,52 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Payments", function () {
-    
+    let acc1
+    let acc2
+    let payments
+
+    beforeEach(async function() {
+        [acc1, acc2] = await ethers.getSigners()
+        const Payments = await ethers.getContractFactory("Payments", acc1)
+        payments = await Payments.deploy()
+        await payments.deployed
+        console.log(payments.address)
+    })
+
+    it("should be deployed", async function() {
+        // https://ethereum-waffle.readthedocs.io/en/latest/matchers.html#proper-address
+        expect(payments.address).to.be.properAddress;
+    })
+
+
+    it("Should have 0 ether by default", async function() {
+        const balance = await payments.currentBalance()
+
+        expect(balance).to.eq(0)
+    })
+
+    it("Should be possible to send funds", async function() {
+        const msg = "hello from hardhat";
+        const amount = 100;
+        const tx = await payments.connect(acc2).pay(msg, { value: amount})
+        
+        await expect(() => tx)
+        .to.changeEtherBalances(
+            [acc2, payments],
+             [-amount, amount]
+             )
+        await tx.wait()
+
+        const balance = await payments.currentBalance()
+
+        console.log(balance)
+
+        const newPayment = await await payments.getPayemtn(acc2.address, 0)
+
+        expect(newPayment.message).to.eq(msg)
+        expect(newPayment.amount).to.eq(amount)
+        expect(newPayment.from).to.eq(acc2.address)
+
+        console.log(newPayment)
+    })
 })
